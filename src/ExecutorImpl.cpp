8568e1ae0fa410272b8b0e01b659b71d12352cce
yuehanzhang <yuehanzhang2005@hotmail.com>
2025-10-22 20:24:08 +0800

    test: Pass initial and default pose test cases

--- src/ExecutorImpl.cpp
+++ src/ExecutorImpl.cpp
@@ -2,15 +2,17 @@
 
 #include <new>
 
-namespace adas{
-    ExecutorImpl::ExecutorImpl(const Pose &pose)noexcept{}
-    Pose ExecutorImpl::Query(void) const noexcept{
-        return Pose{};
+namespace adas
+{
+    ExecutorImpl::ExecutorImpl(const Pose &pose) noexcept : pose(pose) {}
+
+    Pose ExecutorImpl::Query(void) const noexcept
+    {
+        return pose;
     }
 
-    Executor *Executor::NewExecutor(const Pose &pose) noexcept{
-        return new (std::nothrow) ExecutorImpl(pose); //只在C++17下有效
-
+    Executor *Executor::NewExecutor(const Pose &pose) noexcept
+    {
+        return new (std::nothrow) ExecutorImpl(pose); // 只在C++17下有效
     }
 }
-
--- src/ExecutorImpl.hpp
+++ src/ExecutorImpl.hpp
@@ -10,22 +10,23 @@ namespace adas
     */
     class ExecutorImpl : public Executor
     {
-        public:
-        //构造函数，传入初始姿态
+    public:
+        // 构造函数，传入初始姿态
         explicit ExecutorImpl(const Pose &pose) noexcept;
-        //默认析构函数
+        // 默认析构函数
         ~ExecutorImpl(void) noexcept = default;
 
-        //不能拷贝
+        // 不能拷贝
         ExecutorImpl(const ExecutorImpl &) = delete;
-        //不能赋值
+        // 不能赋值
         ExecutorImpl &operator=(const ExecutorImpl &) = delete;
-        public:
-        //查询当前汽车姿态，重写基类的纯虚函数
+
+    public:
+        // 查询当前汽车姿态，重写基类的纯虚函数
         Pose Query(void) const noexcept override;
 
-        private:
+    private:
         Pose pose;
     };
 
-} //namespace adas
+} // namespace adas
