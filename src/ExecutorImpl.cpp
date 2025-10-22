888e77735e860946be928f15c1249f76f358e38f
yuehanzhang <yuehanzhang2005@hotmail.com>
2025-10-22 21:02:21 +0800

    test: Pass turn right command test cases

--- src/ExecutorImpl.cpp
+++ src/ExecutorImpl.cpp
@@ -35,6 +35,13 @@ namespace adas
                 else if(pose.heading == 'W') { pose.heading = 'S'; }
                 else if(pose.heading == 'S') { pose.heading = 'E'; }
             }
+            if(cmd == 'R')
+            {
+                if(pose.heading == 'E') { pose.heading = 'S'; }
+                else if(pose.heading == 'S') { pose.heading = 'W'; }
+                else if(pose.heading == 'W') { pose.heading = 'N'; }
+                else if(pose.heading == 'N') { pose.heading = 'E'; }
+            }
         }
     }
 }
